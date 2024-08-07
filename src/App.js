4403c2c32b71a92d5c8bff2528e9894b0a6519ed
Anas Hany <anashany219@gmail.com>
2024-08-08 02:51:52 +0300

    📜 Build static app with list of friends

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
+import { fireEvent } from "@testing-library/react";
+
 const initialFriends = [
   {
+    id: 933372,
+    name: "Fayroza",
+    image: "https://i.pravatar.cc/48?u=933375",
+    balance: 2000,
+  },
+  {
     id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
+    name: "Islam",
+    image: "https://i.pravatar.cc/48?u=933329",
     balance: -7,
   },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
+
   {
     id: 499476,
-    name: "Anthony",
+    name: "Mattar",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
 
-function App() {
-  return <div>Hello React</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
